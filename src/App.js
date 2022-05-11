29bd6b281b5851433a1044e3a573f1257da6f368
IbraheemShaban <ishaban097@gmail.com>
2022-05-11 20:06:59 +0300

    FINISHED the game

--- src/App.js
+++ src/App.js
@@ -1,23 +1,69 @@
-import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
+
+const randomIntFromInterval = (min, max) => {
+  return Math.floor(Math.random() * (max - min + 1) + min);
+};
+const answer = randomIntFromInterval(1, 10);
 
 function App() {
+  const [number, setNumber] = useState(0);
+  const [tries, setTries] = useState(3);
+  const [messege, setMessege] = useState('');
+  const [mainScreen, setMainScreen] = useState('App');
+  const [winScreen, setWinScreen] = useState('hide');
+  const [lostScreen, setLostScreen] = useState('hide');
+
+  const guess = () => {
+    if (tries > 1) {
+      if (number < 0 || number > 10) {
+        alert('Enter a number between 1-10');
+      } else if (number == answer) {
+        setMainScreen('hide');
+        setWinScreen('App');
+      } else if (number < answer) {
+        setTries(tries - 1);
+        setMessege('The Right Answer is higher');
+      } else if (number > answer) {
+        setTries(tries - 1);
+        setMessege('The Right Answer is lower');
+      }
+    } else {
+      setTries(tries - 1);
+      if (number == answer) {
+        setMainScreen('hide');
+        setWinScreen('App');
+      } else {
+        setTimeout(() => {
+          setMainScreen('hide');
+          setLostScreen('App');
+        }, '210');
+      }
+    }
+  };
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <div className={mainScreen}>
+        <h1>Guessing Game</h1>
+        <h3>Find The Secret Number</h3>
+        <input
+          onChange={(event) => setNumber(event.target.value)}
+          className="input"
+          type="text"
+          placeholder="1-10"
+        />
+        <button onClick={guess}>Guess</button>
+        <h2>{messege}</h2>
+        <h2>tries remaining {tries}</h2>
+      </div>
+      <div className={winScreen}>
+        <h1>You won</h1>
+        <button onClick={() => window.location.reload()}>Restart</button>
+      </div>
+      <div className={lostScreen}>
+        <h1>You Lost</h1>
+        <button onClick={() => window.location.reload()}>Restart</button>
+      </div>
     </div>
   );
 }
